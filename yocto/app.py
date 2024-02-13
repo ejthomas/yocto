31f68fec23aefc912fc188d6eaedfbbbfe3721c6
Edward Thomas <ed.j.thomas@protonmail.com>
2024-02-13 15:00:50 +0000

    Import UserAuthenticator in app.py

--- yocto/app.py
+++ yocto/app.py
@@ -3,13 +3,16 @@ import getpass
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+from auth import UserAuthenticator
+
 app = Flask(__name__)
 
-client = MongoClient("localhost", 27017, username="ejthomas", password=getpass.getpass())
+client = MongoClient("localhost", 27017, username="myUserAdmin", password=getpass.getpass())
 db = client.yocto
 users = db.users
 urls = db.urls
-test = db.test
+
+auth = UserAuthenticator(users)
 
 @app.route("/")
 def index():
