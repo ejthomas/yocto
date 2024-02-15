04272916cc574cfe750b745b59b3356f7e6b94a7
Edward Thomas <ed.j.thomas@protonmail.com>
2024-02-15 13:05:08 +0000

    Basic frontend display and structure

--- yocto/app.py
+++ yocto/app.py
@@ -1,19 +1,21 @@
-import getpass
+# from flask import Flask, render_template
+# from pymongo import MongoClient
 
-from flask import Flask, render_template
-from pymongo import MongoClient
+# from auth import UserAuthenticator
 
-from auth import UserAuthenticator
+# app = Flask(__name__)
 
-app = Flask(__name__)
+# client = MongoClient("localhost", 27017)
+# db = client.yocto
+# users = db.users
+# urls = db.urls
 
-client = MongoClient("localhost", 27017, username="myUserAdmin", password=getpass.getpass())
-db = client.yocto
-users = db.users
-urls = db.urls
+# auth = UserAuthenticator(users)
 
-auth = UserAuthenticator(users)
+# @app.route("/")
+# @app.route("/index/")
+# def index():
+#     return render_template("index.html")
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# if __name__ == "__main__":
+#     app.run("localhost", port=8000, debug=True)
