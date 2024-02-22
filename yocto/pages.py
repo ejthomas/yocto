0abbf91eae574b06d59b4e70cd095e0e0dbe58c5
Edward Thomas <ed.j.thomas@protonmail.com>
2024-02-22 17:02:15 +0000

    Generation of shortened URLs via frontend

--- yocto/pages.py
+++ yocto/pages.py
@@ -1,25 +1,51 @@
+import functools
+
 from flask import (
     Blueprint, 
     render_template, 
     request, 
     redirect, 
     url_for, 
-    session
+    session,
+    g,
 )
 
 from yocto.auth import UserAuthenticator
+from yocto.address import AddressManager
 from yocto.db import get_db
 from yocto.lib.exceptions import (
     UsernameInvalidError,
     PasswordInvalidError,
     UserNotFoundError,
     PasswordMismatchError,
+    UrlInvalidError,
+    UrlExistsError,
 )
+from yocto.lib.utils import USERNAME_IDENTIFIER, LONG_URL_IDENTIFIER, SHORT_ID_IDENTIFIER
 
 bp = Blueprint("pages", __name__)
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user")
+    if user_id is None:
+        g.user = None
+    else:
+        db = get_db()
+        user = db.users.find_one({USERNAME_IDENTIFIER: user_id})
+        g.user = user
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("pages.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 @bp.route("/")
-@bp.route("/index/")
 @bp.route("/index/<disp>/")
 def index(disp=None):
     return render_template("pages/index.html", disp=disp)
@@ -97,29 +123,87 @@ def login_success(user):
 
 @bp.route("/logout/")
 def logout():
-    session.pop("user")
+    session.clear()
     return redirect(url_for("pages.index", disp="user-logged-out"))
 
 @bp.route("/account/")
+@login_required
 def account():
     return render_template("pages/account.html")
 
 @bp.route("/delete/")
+@login_required
 def delete():
     return render_template("pages/delete.html")
 
 @bp.route("/delete/confirmed/")
+@login_required
 def delete_confirmed():
     auth = UserAuthenticator(get_db())
-    try:
-        auth.delete_user(session["user"])
-    except UserNotFoundError:
-        return redirect(url_for("pages.error", message="The account has already been deleted."))
-    except KeyError:
-        return redirect(url_for("pages.error", message="No user logged in."))
-    session.pop("user")
+    auth.delete_user(g.user[USERNAME_IDENTIFIER])
+    session.clear()
     return redirect(url_for("pages.index", disp="account-delete-success"))
 
 @bp.route("/error/")
 def error():
     return render_template("pages/error.html", message=request.args["message"])
+
+@bp.route("/create/", methods=["POST", "GET"])
+@login_required
+def create():
+    if request.method == "POST":
+        long_url = request.form["url"]
+        am = AddressManager(get_db())
+        short_id = am.generate_short_id()
+        try:
+            am.store_url_and_id(long_url, short_id, g.user[USERNAME_IDENTIFIER])
+        except UrlInvalidError:
+            return render_template(
+            "pages/create.html",
+            form={"url": ""},
+            short_url=None,
+            message="Input is not a valid web address.",
+        )
+        except UrlExistsError:
+            db = get_db()
+            short_id = db.urls.find_one({LONG_URL_IDENTIFIER: long_url})[SHORT_ID_IDENTIFIER]
+            return render_template(
+                "pages/create.html",
+                form={"url": ""},
+                short_url=am.compose_shortened_url(url_for("pages.index", _external=True), short_id),
+                message=None,
+            )
+        # UserNotFoundError should be impossible due to login_required decorator
+        return render_template(
+            "pages/create.html",
+            form={"url": ""},
+            short_url=am.compose_shortened_url(url_for("pages.index", _external=True), short_id),
+            message=None,
+        )
+    else:
+        return render_template(
+            "pages/create.html",
+            form={"url": ""},
+            short_url=None,
+            message=None,
+        )
+    
+@bp.route("/my-links/")
+@login_required
+def my_links():
+    am = AddressManager(get_db())
+    addresses = am.lookup_user_urls(g.user[USERNAME_IDENTIFIER])
+    return render_template(
+        "pages/my_links.html", 
+        links=[
+            {
+                "long": address[LONG_URL_IDENTIFIER], 
+                "short": am.compose_shortened_url(
+                    url_for("pages.index", _external=True), 
+                    address[SHORT_ID_IDENTIFIER]
+                ) 
+            }
+            for address in addresses
+        ]
+    )
+    
